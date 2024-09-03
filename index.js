2d05d0a90ade6647abba63e4e24e964aa6c0a4ba
Zhenya Tikhonov <zhenya.tikhonov@codefresh.io>
2024-09-03 18:33:27 +0400

    refactor: refresh code

--- index.js
+++ index.js
@@ -1,94 +1,118 @@
+// @ts-check
+
 /**
- * @param {Object}              [query={}]
- * @param {Object}              [options={}]
- * @param {Object|String}         [options.select]
- * @param {Object|String}         [options.sort]
- * @param {Array|Object|String}   [options.populate]
- * @param {Boolean}               [options.lean=false]
- * @param {Boolean}               [options.leanWithId=true]
- * @param {Number}                [options.offset=0] - Use offset or page to set skip position
- * @param {Number}                [options.page=1]
- * @param {Number}                [options.limit=10]
+ * @typedef {Object}                    BaseOptions
+ * @property {Object | string}          [select]
+ * @property {Object | string}          [sort]
+ * @property {Array | Object | string}  [populate]
+ * @property {boolean}                  [lean=false]
+ * @property {boolean}                  [leanWithId=true]
+ * @property {number}                   [limit]
+ * 
+ * @typedef {Object}                    OffsetProps
+ * @property {number}                   [offset=0] Use either `offset` or `page` to set skip position
+ * @property {never}                    [page]
+ * @typedef {OffsetProps & BaseOptions} OffsetOptions
+ * 
+ * @typedef {Object}                    PageProps
+ * @property {never}                    [offset]
+ * @property {number}                   [page=1] Use either `offset` or `page` to set skip position
+ * @typedef {PageProps & BaseOptions}   PageOptions
+ * 
+ * @typedef {OffsetOptions|PageOptions} Options
  *
+ * @typedef {Required<Pick<Options, 'lean' | 'leanWithId' | 'limit'>>} DefaultOptions
+ *
+ * @typedef {import('mongoose').FilterQuery<any>}   Filter
+ * @typedef {import('mongoose').Schema}             Schema
+ * @typedef {import('mongoose').Model}              Model
+ * @typedef {import('mongoose').Query}              Query
+ */
+
+/** @type {DefaultOptions} */
+const defaultOptions = {
+    lean: false,
+    leanWithId: true,
+    limit: 10,
+};
+
+/**
+ * @this {Model}
+ * @param {Filter} [filter={}]
+ * @param {Options} [opts] See {@link defaultOptions default options} for default values
  * @returns {Promise}
  */
-function paginate(query, options) {
-    query   = query || {};
-    options = Object.assign({}, paginate.options, options);
+async function paginate(filter = {}, opts = {}) {
+    const options = {
+        ...defaultOptions,
+        ...paginate.options,
+        ...opts,
+    };
 
-    var select     = options.select;
-    var sort       = options.sort;
-    var populate   = options.populate;
-    var lean       = options.lean || false;
-    var leanWithId = options.hasOwnProperty('leanWithId') ? options.leanWithId : true;
+    const { lean, leanWithId, limit, select, sort, populate } = options;
 
-    var limit = options.hasOwnProperty('limit') ? options.limit : 10;
-    var skip, offset, page;
-
+        let skip, offset, page;
     if (options.hasOwnProperty('offset')) {
         offset = options.offset;
-        skip   = offset;
+        skip = offset;
     } else if (options.hasOwnProperty('page')) {
         page = options.page;
         skip = (page - 1) * limit;
     } else {
         offset = 0;
-        page   = 1;
-        skip   = offset;
+        page = 1;
+        skip = offset;
     }
 
-    let docsPromise = Promise.resolve([]);
-    const countPromise = Object.keys(query).length === 0
+    const countPromise = Object.keys(filter).length === 0
         ? this.estimatedDocumentCount().exec()
-        : this.countDocuments(query).exec();
+        : this.countDocuments(filter).exec();
+    /** @type {Promise<any[]>}  */
+    let docsPromise = Promise.resolve([]); // in case limit is 0
 
-    if (limit) {
-        var query = this.find(query)
-                        .select(select)
-                        .sort(sort)
-                        .skip(skip)
-                        .limit(limit)
-                        .lean(lean);
+    if (limit !== 0) {
+        /** @type {Query} */
+        const query = this.find(filter)
+            .select(select)
+            .sort(sort)
+            .skip(skip)
+            .limit(limit)
+            .lean(lean);
 
         if (populate) {
-            [].concat(populate).forEach(function(item) {
-                query.populate(item);
-            });
+            [].concat(populate).forEach((item) => query.populate(item));
         }
 
         docsPromise = query.exec();
 
         if (lean && leanWithId) {
-            docsPromise = docsPromise.then(function(docs) {
-                docs.forEach(function(doc) {
+            docsPromise = docsPromise.then((docs) => {
+                return docs.map((doc) => {
                     doc.id = String(doc._id);
+                    return doc;
                 });
-
-                return docs;
             });
         }
     }
 
-    return Promise.all([docsPromise, countPromise])
-        .then(function([docs, total]) {
-            return {
-                docs,
-                total,
-                limit,
-                ...(offset !== undefined && { offset }),
-                ...(page !== undefined && {
-                    page,
-                    pages: Math.ceil(total / limit) || 1,
-                }),
-            };
-        });
+    const [docs, total] = await Promise.all([docsPromise, countPromise]);
+    return {
+        docs,
+        total,
+        limit,
+        ...(offset !== undefined && { offset }),
+        ...(page !== undefined && {
+            page,
+            pages: Math.ceil(total / limit) || 1,
+        }),
+    };
 }
+/** @type {Options} */
+paginate.options = defaultOptions;
 
-/**
- * @param {Schema} schema
- */
-module.exports = function(schema) {
-    schema.statics.paginate = paginate;
+/** @param {Schema} schema */
+module.exports = function (schema) {
+    schema.static('paginate', paginate);
 };
 
 module.exports.paginate = paginate;
